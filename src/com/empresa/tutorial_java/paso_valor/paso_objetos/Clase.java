0ef5cb8c4649a4c7cd291c36b99dff5c86ef14d0
Jose <Jose.lomba.goce03@gmail.com>
2023-05-24 21:22:42 +0200

    Merge branch 'valor_referencia'

--- src/com/empresa/tutorial_java/paso_valor/paso_objetos/Clase.java
+++ src/com/empresa/tutorial_java/paso_valor/paso_objetos/Clase.java
@@ -10,6 +10,6 @@ class Clase {
     }
     @Override
     public String toString() {
-        return "Objeto { i=" + entero + ", a='" + string + "\'}";
+        return "Objeto { i=" + entero + ", a='" + string + "'}";
     }
 }
